3d1b2b2dddf178b04acb5fdd16cf690299e3f64a
aya munadhil <45121612+ay0o5h@users.noreply.github.com>
2021-08-25 11:52:28 +0300

    edit style

---
+++ components/mainLayout2.jsx
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import cover from '../public/images/cover.svg';
+import Link from "next/link";
+import {ArrowRightOutlined} from '@ant-design/icons'
+const MainLayout2 = () => {
+    return (
+        <div className="main">
+            <div className="left">
+                <h1>Book Your Table Now</h1>
+                <p>Get restaurant table reservations Better online than keep a busy phone line</p> 
+                <Link href="/signup"><a className="btn"> Get Started <ArrowRightOutlined style={{ fontSize: '16px'}} /> </a></Link>
+                </div>
+            <div className="right">
+            <Image className="cover" src={cover}  /> 
+            </div>
+        </div>
+    )
+}
+export default MainLayout2
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,10 @@
-import Navbar from '../components/navbar'
-import MainLayout from '../components/mainLayout'
+import MainLayout from '../components/MainLayout'
 import Restaurants from '../components/restaurants'
 import Footer from '../components/footer'
 export default function Home() {
   return (
     <>
-      <Navbar/>
       <MainLayout/>
-      <Restaurants/>
-      <Footer/>
     </>
   )
 }
